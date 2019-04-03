1afeb8fde903f38b17bad506e4761558cf4415da
SEBSOUNdS <sebastian.udden@gmail.com>
2019-04-03 19:27:48 +0200

    Setup of new repository

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # my-web-tools-api
+
+## Things needed for local deployment
+
+-   nodemon --exec babel-node src/index.js
+-   .env file
+    -   PORT=
+    -   MONGODB_URI=
+    -   ERASE_DB_ON_SYNC=
+    -   SEED_DB_WITH_USERS_AND_MESSAGES=
+
+## Things needed for remote deployment
+
+-   Babel build (No ES6)
+-   Procfile
+-   heroku config variables
+    -   PORT=
+    -   MONGODB_URI=
+    -   ERASE_DB_ON_SYNC=
+    -   SEED_DB_WITH_USERS_AND_MESSAGES=
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import { createUsersWithMessages } from './mock';
+import models, { connectDb } from './models';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(async (req, res, next) => {
+    req.context = {
+        models,
+        me: await models.User.findByLogin(req.query.token || 'Unauthorized'),
+    };
+    next();
+});
+
+app.use('/session', routes.session);
+app.use('/users', routes.users);
+app.use('/messages', routes.messages);
+
+connectDb().then(async () => {
+    // if (process.env.ERASE_DB_ON_SYNC) {
+    //     await Promise.all([
+    //         models.User.deleteMany({}),
+    //         models.Message.deleteMany({}),
+    //     ]);
+    // }
+
+    // if (process.env.SEED_DB_WITH_USERS_AND_MESSAGES) {
+    //     createUsersWithMessages();
+    // }
+    app.listen(process.env.PORT || 3000, () =>
+        console.log(`Example app listening on port ${process.env.PORT}...`),
+    );
+});
